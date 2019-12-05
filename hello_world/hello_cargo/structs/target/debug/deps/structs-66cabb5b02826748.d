5e6a50d076405d1c65be6298871b8a78a5d7fc77
HarshitRuwali <harshitruwali@outlook.com>
2019-12-05 17:20:44 +0530

    Learning structures

---
+++ hello_world/hello_cargo/structs/target/debug/deps/structs-66cabb5b02826748.d
@@ -0,0 +1,5 @@
+/Users/harshitruwali/Rust/hello_world/hello_cargo/structs/target/debug/deps/structs-66cabb5b02826748: src/main.rs
+
+/Users/harshitruwali/Rust/hello_world/hello_cargo/structs/target/debug/deps/structs-66cabb5b02826748.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/hello_cargo/structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/Users/harshitruwali/Rust/hello_world/hello_cargo/structs/target/debug/structs: /Users/harshitruwali/Rust/hello_world/hello_cargo/structs/src/main.rs
